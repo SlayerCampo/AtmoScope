7da0a0eb2407006a0bcfe498764389307ae05814
Alejandro Campo Solarte <alejandro2007campo1@gmail.com>
2026-09-07 20:51:44 -0500

    feat: implement zustand global store for timeline and region state

--- src/store/useAppStore.ts
+++ src/store/useAppStore.ts
@@ -1,5 +1,19 @@
 import { create } from 'zustand'
 
-interface AppState {}
+interface AppState {
+  selectedYear: number
+  activeRegion: string | null
+  eventFilter: string
+  setSelectedYear: (year: number) => void
+  setActiveRegion: (region: string | null) => void
+  setEventFilter: (filter: string) => void
+}
 
-export const useAppStore = create<AppState>(() => ({}))
+export const useAppStore = create<AppState>((set) => ({
+  selectedYear: 2024,
+  activeRegion: null,
+  eventFilter: 'ALL',
+  setSelectedYear: (year) => set({ selectedYear: year }),
+  setActiveRegion: (region) => set({ activeRegion: region }),
+  setEventFilter: (filter) => set({ eventFilter: filter }),
+}))
